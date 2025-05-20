1dd33802a8dd3d12d209c6d473f0dd1781169745
kashishj197 <kashishj197@gmail.com>
2025-05-20 13:29:39 +0530

    last commit

--- backend/db/neo4j.py
+++ backend/db/neo4j.py
@@ -2,9 +2,9 @@ from neo4j import GraphDatabase
 import os
 
 # Use environment variables or hardcode for now
-NEO4J_URI = os.getenv("NEO4J_URI", "neo4j+s://4e4b67c6.databases.neo4j.io")
-NEO4J_USER = os.getenv("NEO4J_USER", "neo4j")
-NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", "SOSlZAR32IoCWe5gHzqYkSKrXzfWx_QFtOhHGtu4n2g")
+NEO4J_URI = os.getenv("NEO4J_URI", "Your connection URI here")
+NEO4J_USER = os.getenv("NEO4J_USER", "Your username here")
+NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", "Your password here")
 
 driver = GraphDatabase.driver(NEO4J_URI, auth=(NEO4J_USER, NEO4J_PASSWORD))
 
